786923e12ca2dc266ce4d8595f517372d9532da8
Rustdj <rustdj@icloud.com>
2021-09-15 21:25:54 +0300

    react-blog

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+import './app-header.css';
+import styled from 'styled-components';
+
+const Header = styled.div `
+    display: flex;
+    align-items: flex-end;
+	justify-content: space-between;
+    h1 {
+        font-size: 26px;
+        color: ${props => props.colored ? 'blue' : 'black'}; // тернатный оператор!
+        :hover {
+            color: orange;
+            transition: 0.7s;
+            cursor: pointer;
+        }
+    }
+    h2 {
+        font-size: 1.2rem;
+	    color: grey;
+    }
+`
+
+const AppHeader = () => {
+    return ( 
+        <Header colored>
+            <h1> Rustam Raikerus</h1> 
+            <h2> 5 записей, из них понравилось 0</h2> 
+        </Header>
+    )
+}
+
+export default AppHeader;
